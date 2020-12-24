66ade3412220d703630ed8a5f2e2bf6f60cd6d47
talaamin <76010372+talaamin@users.noreply.github.com>
2020-12-24 11:27:06 +0200

    Add files via upload

---
+++ Hampster.java
@@ -0,0 +1,30 @@
+package com.serenitydojo;
+
+public class Hampster extends Pet {
+	private String favoriteGame;
+	private int age;
+
+	public Hampster(String name, String favoriteGame, int age) {
+		super(name);
+		this.favoriteGame = favoriteGame;
+		this.age = age;
+	}
+
+	public String getFavoriteGame() {
+		return favoriteGame;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String makeNoise() {
+		return "Squeak";
+	}
+
+	@Override
+	public String play() {
+		return "runs in wheel";
+	}
+}
---
+++ Pet.java
@@ -0,0 +1,21 @@
+package com.serenitydojo;
+
+public abstract class Pet {
+	private String name;
+
+	public Pet(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String goForWalks() {
+		return "walk walk walk";
+	}
+
+	public abstract String makeNoise();
+
+	public abstract String play();
+}
