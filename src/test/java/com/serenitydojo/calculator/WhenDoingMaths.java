e2e31c9ec85ed528307ca837b5b7fa21eed8f930
talaamin <76010372+talaamin@users.noreply.github.com>
2021-01-25 19:24:02 +0200

    Calculator challenge

--- src/test/java/com/serenitydojo/calculator/WhenDoingMaths.java
+++ src/test/java/com/serenitydojo/calculator/WhenDoingMaths.java
@@ -12,39 +12,38 @@ public class WhenDoingMaths {
     public void shouldReturnZeroForAnEmptyString() {
         assertThat(calculator.evaluate("")).isEqualTo(0);
     }
+    @Test(expected = IllegalMathsOperatorException.class)
+    public void shouldReportNonSupportedOperations() {
+        calculator.evaluate("1 ^ 2");
+    }
 
-//    @Test(expected = IllegalMathsOperatorException.class)
-//    public void shouldReportNonSupportedOperations() {
-//        calculator.evaluate("1 ^ 2");
-//    }
-//
-//    @Test
-//    public void shouldReturnTheValueOfASingleNumber() {
-//        assertThat(calculator.evaluate("1")).isEqualTo(1);
-//    }
-//
-//    @Test
-//    public void shouldAddTwoNumbers() {
-//        assertThat(calculator.evaluate("1 + 1")).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void shouldAddThreeNumbers() {
-//        assertThat(calculator.evaluate("1 + 2 + 3")).isEqualTo(6);
-//    }
-//
-//    @Test
-//    public void shouldAlsoSubtract() {
-//        assertThat(calculator.evaluate("10 - 6")).isEqualTo(4);
-//    }
-//
-//    @Test
-//    public void shouldAddAndSubtract() {
-//        assertThat(calculator.evaluate("10 + 5 - 6")).isEqualTo(9);
-//    }
-//
-//    @Test
-//    public void shouldMultiplyNumbers() {
-//        assertThat(calculator.evaluate("10 * 5")).isEqualTo(50);
-//    }
+    @Test
+    public void shouldReturnTheValueOfASingleNumber() {
+        assertThat(calculator.evaluate("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldAddTwoNumbers() {
+        assertThat(calculator.evaluate("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldAddThreeNumbers() {
+        assertThat(calculator.evaluate("1 + 2 + 3")).isEqualTo(6);
+    }
+
+    @Test
+    public void shouldAlsoSubtract() {
+        assertThat(calculator.evaluate("10 - 6")).isEqualTo(4);
+    }
+
+    @Test
+    public void shouldAddAndSubtract() {
+        assertThat(calculator.evaluate("10 + 5 - 6")).isEqualTo(9);
+    }
+
+    @Test
+    public void shouldMultiplyNumbers() {
+        assertThat(calculator.evaluate("10 * 5")).isEqualTo(50);
+    }
 }
